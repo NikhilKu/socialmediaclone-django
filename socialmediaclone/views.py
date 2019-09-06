6fb8bde3d55b53d755925755e2a8394a85835cd4
Nikhil Kumar <nikhil.skumar@hotmail.com>
2019-09-06 22:22:19 +0200

    Course finished

---
+++ socialmediaclone/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import TemplateView
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+
+class TestPage(TemplateView):
+    template_name = 'logged_in.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
